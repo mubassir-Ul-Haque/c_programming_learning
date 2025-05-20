cdd02b44a1edd9f8d519b26a0a741356e5a9f872
mubassirulhaque233822 <mubassirulhaque233822@gmail.com>
2025-05-20 05:08:52 +0000

    Initial commit

---
+++ calculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int mark1, mark2, mark3, mark4, mark5;
+              int total;
+ 
+    cout << "Enter marks for subject 1: ";
+    cin >> mark1;
+
+    cout << "Enter marks for subject 2: ";
+    cin >> mark2;
+
+            cout << "Enter marks for subject 3: ";
+         cin >> mark3;
+
+    cout << "Enter marks for subject 4: ";
+                cin >> mark4;
+
+    cout << "Enter marks for subject 5: ";
+    cin >>        mark5;
+
+    total = mark1 + mark2 + mark3 + mark4 + mark5;
+
+           cout << "  Total Marks = " << total << endl;
+
+    return 0;
+}
+
+
+
