463b1a8ac5fd05aa086f6ed765d071d877796dcf
mubassirulhaque233822 <mubassirulhaque233822@gmail.com>
2025-05-20 21:52:14 +0000

    Learning Phase

---
+++ c-programming/CalculatingPercentageOfAllSubjects.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    float math=80;
+    float gk=80;
+    float biology=80;
+    float physiscs=80;
+    float chemistry=80;
+    float percentage=(math+gk+biology+physiscs+chemistry)/5;
+    printf("Percentage of 5 subject is %f",percentage);
+    return 0;
+}
