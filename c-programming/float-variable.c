4853df68a6760d0b98e11e9c1e3ebdf587e716bd
mubassirulhaque233822 <mubassirulhaque233822@gmail.com>
2025-05-20 19:45:11 +0000

    learning phase

--- c-programming/float-variable.c
+++ c-programming/float-variable.c
@@ -1,1 +1,14 @@
-git aff
+#include <stdio.h>
+
+int main() {
+
+     float a = 5.5;
+    float b = 2.5;
+
+    printf("%f\n", a + b);
+    printf("%f\n", a - b);
+    printf("%f\n", a * b);
+    printf("%f\n", a / b);
+
+    return 0;
+}
