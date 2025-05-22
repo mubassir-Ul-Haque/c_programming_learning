2aec4ef0fe1cf333b5170c61dd41c24df4ae6ffe
mubassirulhaque233822 <mubassirulhaque233822@gmail.com>
2025-05-22 04:25:16 +0000

    For Practice

--- c-programming/simple_interest.c
+++ c-programming/simple_interest.c
@@ -1,20 +1,21 @@
 #include<stdio.h>
 
-int main(){
+int main() {
 
     float p, r, t, si;
 
     printf("Enter Value Of P :\n");
-    scanf("%f",&p);
+    scanf("%f", &p);
 
-    printf("Enter Value Of r :\n");
-    scanf("%f",&r);
+    printf("Enter Value Of R :\n");
+    scanf("%f", &r);
 
+    printf("Enter Value Of T :\n");
+    scanf("%f", &t);
 
-    printf("Enter Value Of t :\n");
-    scanf("%f",&t);
+    si = (p * r * t) / 100;
 
-    si=(p*r*t)/100;
-    printf("Your Simple Interest Is %f\n" :si);
+    printf("Your Simple Interest Is: %f\n", si);
+
     return 0;
 }
