fb5c5b6b8eb4aba479062a26a0061c7ef777b963
mubassirulhaque233822 <mubassirulhaque233822@gmail.com>
2025-05-23 12:55:17 +0000

    Initial commit

--- c-programming/AreaOfaCircle.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int  main(){
-    int r=5;
-    float v=3.1416*r*r;
-    printf("The area of the circle is :%f",v);
-    return 0;
-
-}
--- c-programming/Char_data_type_basic.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-
-char ch = '#';
-printf("%c",ch);
-
-return 0;
-}
